52e9f57c84192d881eb75946b12530a8f84a911d
Javier Del Rio <javier.ignaciodr1@gmail.com>
2024-06-19 23:17:31 -0300

    NestJS Fundamentals

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from 'express';
+
+export function LoggerMiddleware(req: Request, res: Response, next: NextFunction) {
+
+  const actualDate = new Date();
+  const date = actualDate.toLocaleDateString();
+  const time = actualDate.toLocaleTimeString();
+  const method = req.method;
+  const url = req.url;
+  console.log(`${date} ${time} ${method} ${url}`);
+  next();
+}
