5e615df5cf4c0f90763761fd736f1b7e0e2dcb8a
Javier Del Rio <javier.ignaciodr1@gmail.com>
2024-07-02 23:59:51 -0300

    NestJS & TypeORM

---
+++ src/entities/orderdetails.entity.ts
@@ -0,0 +1,44 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Products  } from "./products.entity";
+import { Orders } from "./orders.entity";
+
+@Entity({
+  name: "ORDERDETAILS",
+})
+export class OrderDetails {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({
+    type: "decimal",
+    precision: 10,
+    scale: 2,
+  })
+  price: number;
+
+  @OneToOne(() => Orders, (order) => order.orderDetails)
+  @JoinColumn({ name: "order_id" })
+  order: Orders;
+
+  @ManyToMany(() => Products)
+  @JoinTable({ 
+    name: "ORDERDETAILS_PRODUCTS",
+    joinColumn: {
+      name: "product_id",
+      referencedColumnName: "id",
+    },
+    inverseJoinColumn: {
+      name: "orderdetail_id",
+      referencedColumnName: "id",
+    }
+  })
+  products: Products[];
+}
