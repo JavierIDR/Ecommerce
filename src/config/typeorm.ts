809f0f515898465b45e796f7185d5648edb0d724
Javier Del Rio <javier.ignaciodr1@gmail.com>
2024-08-05 12:46:52 -0300

    Docker

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -8,6 +8,7 @@ const config = {
   type: "postgres",
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
+  // host: "postgresdb",
   port: process.env.DB_PORT as unknown as number,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
